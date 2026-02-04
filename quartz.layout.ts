333312f7cfc274c4551ffec119bfc688d275d34e
NetYeti <growlfd@gmail.com>
2026-02-03 22:40:51 -0800

    Adding recentnotes to right side

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -305,6 +305,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.RecentNotes(),
   ],
 }
 
